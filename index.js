2dd5a90cd3d9e2f7c95a0709a22959cba5892e62
harsh661 <plasmaharsh@gmail.com>
2023-09-04 19:23:35 +0530

    change error message

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/api/:date", (req, res) => {
 
     res.json({ unix: timestamp, utc: utcDateString })
   } else {
-    res.status(400).json({ error: "Invalid input format" })
+    res.status(400).json({ error: "Invalid date" })
   }
 })
 
